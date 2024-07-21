b3d7ae3c86be49f119ef0d30aa8dc76f3c0610bf
Chandra Arifin <chandra_arifin@outlook.com>
2024-07-21 11:33:17 +0700

    commit

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,10 +1,45 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
+import "../styles/index.css";
 
 function RootLayout() {
   return (
     <>
-      <Link to="/">Home</Link> | <Link to="Blog">Blog</Link> |{" "}
-      <Link to="About">About</Link>
+      <NavLink
+        className={({ isActive, isPending }) =>
+          isActive ? "active" : isPending ? "pending" : ""
+        }
+        style={{
+          marginRight: "5px",
+        }}
+        to="/"
+      >
+        Home
+      </NavLink>{" "}
+      |
+      <NavLink
+        className={({ isActive, isPending }) =>
+          isActive ? "active" : isPending ? "pending" : ""
+        }
+        style={{
+          marginRight: "5px",
+          marginLeft: "5px",
+        }}
+        to="Blog"
+      >
+        Blog
+      </NavLink>
+      |
+      <NavLink
+        className={({ isActive, isPending }) =>
+          isActive ? "active" : isPending ? "pending" : ""
+        }
+        style={{
+          marginLeft: "5px",
+        }}
+        to="About"
+      >
+        About
+      </NavLink>
       <p />
       <Outlet />
     </>
